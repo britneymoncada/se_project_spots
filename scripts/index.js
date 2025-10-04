447a79772f7b699394a18affa39bdac9aa4e2ab1
Britney Moncada <bmon3.1415@gmail.com>
2025-10-04 13:53:33 -0400

    Add open/close feature to Modals

--- scripts/index.js
+++ scripts/index.js
@@ -1,32 +1,37 @@
 const initialCards = [
   {
-    name: "Place name here",
-    link: "https://image-url-here.jpg",
+    name: "Horizontal Test",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/7-photo-by-griffin-wooldridge-from-pexels.jpg",
   },
 
   {
-    name: "Another place name",
-    link: "https://another-image-url.jpg",
+    name: "Val Thorens",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 
   {
-    name: "Another place name",
-    link: "https://another-image-url.jpg",
+    name: "Restaurant terrace",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
 
   {
-    name: "Another place name",
-    link: "https://another-image-url.jpg",
+    name: "An outdoor cafe",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
 
   {
-    name: "Another place name",
-    link: "https://another-image-url.jpg",
+    name: "A very long bridge, over the forest and through the trees",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
 
   {
-    name: "Another place name",
-    link: "https://another-image-url.jpg",
+    name: "Tunnel with morning light",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+  },
+
+  {
+    name: "Mountain house",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
 
@@ -50,6 +55,56 @@ const imageLink = newPostForm.querySelector("#url");
 const imageCaption = newPostForm.querySelector("#caption-text");
 const newPostSaveBtn = newPostModal.querySelector(".modal__save-button");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-button");
+
+const cardsContainer = document.querySelector(".cards__list");
+const cardTemplate = cardsContainer.querySelector("#card-template");
+
+/* PREVIEW MODAL NAMES*/
+
+const cardPreviewModal = document.querySelector("#modal__card-preview");
+const cardPreviewClose = cardPreviewModal.querySelector(
+  ".modal__preview-close"
+);
+const cardPreviewModalImage = cardPreviewModal.querySelector(".modal__link");
+const cardPreviewCaption = cardPreviewModal.querySelector(".modal__caption");
+
+function getCardElement(data) {
+  const cardElement = cardTemplate.content.cloneNode(true);
+
+  const cardElementImage = cardElement.querySelector(".card__image");
+  cardElementImage.src = data.link;
+  cardElementImage.alt = data.name;
+
+  const cardElementName = cardElement.querySelector(".card__name");
+  cardElementName.textContent = data.name;
+
+  const cardLikeBtn = cardElement.querySelector(".card__like-button");
+
+  cardLikeBtn.addEventListener("click", () => {
+    cardLikeBtn.classList.toggle("card__like-button");
+    cardLikeBtn.classList.toggle("card__like-button-clicked");
+  });
+
+  /* PREVIEW MODAL FUNCTION */
+
+  cardElementImage.addEventListener("click", () => {
+    cardPreviewCaption.textContent = data.name;
+    cardPreviewModalImage.src = data.link;
+    cardPreviewModalImage.alt = data.name;
+    openModal(cardPreviewModal);
+  });
+
+  cardPreviewClose.addEventListener("click", () => {
+    closeModal(cardPreviewModal);
+  });
+
+  const cardDeleteBtnElement = cardElement.querySelector(".card__delete-btn");
+  cardDeleteBtnElement.addEventListener("click", () => {
+    cardDeleteBtnElement.closest(".card").remove();
+  });
+
+  return cardElement;
+}
 
 function openModal(modal) {
   modal.classList.add("modal_opened");
@@ -80,16 +135,27 @@ editProfileCloseBtn.addEventListener("click", () => {
   closeModal(editProfileModal);
 });
 
-function NewCardPost(evt) {
+function newCardPost(evt) {
   evt.preventDefault();
   openModal(newPostModal);
 }
 
-newPostBtn.addEventListener("click", NewCardPost);
+newPostBtn.addEventListener("click", newCardPost);
 newPostForm.addEventListener("submit", (evt) => {
   evt.preventDefault();
-  console.log(imageLink.value);
-  console.log(imageCaption.value);
+
+  console.log("card created");
+
+  const data = {
+    name: imageCaption.value.trim(),
+    link: imageLink.value.trim(),
+  };
+
+  const newCard = getCardElement(data);
+
+  cardsContainer.prepend(newCard);
+
+  newPostForm.reset();
   closeModal(newPostModal);
 });
 
@@ -97,6 +163,7 @@ newPostCloseBtn.addEventListener("click", () => {
   closeModal(newPostModal);
 });
 
-initialCards.forEach(function (card) {
-  console.log(card.name);
+initialCards.forEach((data) => {
+  const clonedCard = getCardElement(data);
+  cardsContainer.prepend(clonedCard);
 });
